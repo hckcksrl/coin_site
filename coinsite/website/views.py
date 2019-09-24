d8e79fb033813d22d3152b5c85b8d422fee0f42a
hckcksrl <hckcksrl@gmail.com>
2019-09-24 22:59:06 +0900

    commit

--- coinsite/website/views.py
+++ coinsite/website/views.py
@@ -1,7 +1,1 @@
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework import status
-from rest_framework.request import Request
-import json
-import requests
 
