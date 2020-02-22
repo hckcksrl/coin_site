ec1f483b9b32c8989346a92ab9ef65bb76fff1f3
hckcksrl <hckcksrl@gmail.com>
2020-02-22 19:41:30 +0900

    commit

--- coinsite/website/urls.py
+++ coinsite/website/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('coin/<str:site>/', views.CoinOne.as_view()),
+    path('coin/<str:site>/', views.GetCoin.as_view()),
     path('search', views.Search.as_view())
 ]
